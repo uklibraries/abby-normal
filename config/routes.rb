ec440b014e3294667129231bd42f3210ff64a2db
Michael Slone <m.slone@gmail.com>
2014-08-29 11:07:04 -0400

    add batches route to API

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ AbbyNormal::Application.routes.draw do
 
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :batches
       resources :packages
 
       match 'packages/:id' => 'packages#update', :via => :post
